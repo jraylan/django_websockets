aec315938c1c79c445018854979d2d6b70f2fdbf
Raylan <jeffersonraylan@gmail.com>
2023-04-13 16:00:02 -0300

    Fix protobuff and Bump version

--- src/django_websockets/transport/proto/wstransport_pb2.pyi
+++ src/django_websockets/transport/proto/wstransport_pb2.pyi
@@ -5,12 +5,14 @@ from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Opti
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class WSMessage(_message.Message):
-    __slots__ = ["message", "type"]
+    __slots__ = ["message", "params", "type"]
     MESSAGE_FIELD_NUMBER: _ClassVar[int]
+    PARAMS_FIELD_NUMBER: _ClassVar[int]
     TYPE_FIELD_NUMBER: _ClassVar[int]
     message: str
+    params: str
     type: str
-    def __init__(self, type: _Optional[str] = ..., message: _Optional[str] = ...) -> None: ...
+    def __init__(self, type: _Optional[str] = ..., message: _Optional[str] = ..., params: _Optional[str] = ...) -> None: ...
 
 class WSResponse(_message.Message):
     __slots__ = ["ack"]
@@ -19,11 +21,9 @@ class WSResponse(_message.Message):
     def __init__(self, ack: bool = ...) -> None: ...
 
 class WSSendMessageRequest(_message.Message):
-    __slots__ = ["group", "message", "params"]
+    __slots__ = ["group", "message"]
     GROUP_FIELD_NUMBER: _ClassVar[int]
     MESSAGE_FIELD_NUMBER: _ClassVar[int]
-    PARAMS_FIELD_NUMBER: _ClassVar[int]
     group: str
     message: WSMessage
-    params: str
-    def __init__(self, group: _Optional[str] = ..., message: _Optional[_Union[WSMessage, _Mapping]] = ..., params: _Optional[str] = ...) -> None: ...
+    def __init__(self, group: _Optional[str] = ..., message: _Optional[_Union[WSMessage, _Mapping]] = ...) -> None: ...
